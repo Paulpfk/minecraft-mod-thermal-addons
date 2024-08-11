a2762e392753325df0feee7d0dca6a7ab70493b1
CATa5trophe <pfk.timo612@gmail.com>
2024-08-11 15:55:36 +0200

    fix(cleanup): removed worldheater item

--- src/main/java/com/cata5trophe/thermaladdons/registration/ItemRegistration.java
+++ src/main/java/com/cata5trophe/thermaladdons/registration/ItemRegistration.java
@@ -1,15 +1,10 @@
 package com.cata5trophe.thermaladdons.registration;
 
 import com.cata5trophe.thermaladdons.ThermalAddonsMod;
-import com.cata5trophe.thermaladdons.items.WorldHeaterItem;
-import com.cata5trophe.thermaladdons.tab.ThermalAddonsCreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
-import net.minecraftforge.registries.RegistryObject;
 
 public class ItemRegistration {
     public static DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, ThermalAddonsMod.MOD_ID);
-
-    public static final RegistryObject<Item> WORLD_HEATER = ITEMS.register(WorldHeaterItem.NAME, () -> new WorldHeaterItem(ThermalAddonsCreativeModeTab.THERMAL_ADDONS_TAB));
 }
