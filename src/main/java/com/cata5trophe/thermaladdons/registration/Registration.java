22767392a4abbf91fb15a71e1a74c6eebe0ceed5
CATa5trophe <pfk.timo612@gmail.com>
2024-08-15 01:45:57 +0200

    fix(cleanup): cleanup

--- src/main/java/com/cata5trophe/thermaladdons/registration/Registration.java
+++ src/main/java/com/cata5trophe/thermaladdons/registration/Registration.java
@@ -9,20 +9,8 @@ import static com.cata5trophe.thermaladdons.registration.ItemRegistration.ITEMS;
 public class Registration {
 
     public static void registerAll(IEventBus modEventbus) {
-        registerItems(modEventbus);
-        registerBlocks(modEventbus);
-        registerBlockEntities(modEventbus);
-    }
-
-    private static void registerItems(IEventBus modEventbus) {
         ITEMS.register(modEventbus);
-    }
-
-    private static void registerBlocks(IEventBus modEventbus) {
         BLOCKS.register(modEventbus);
-    }
-
-    private static void registerBlockEntities(IEventBus modEventbus) {
         BLOCK_ENTITIES.register(modEventbus);
     }
 }
